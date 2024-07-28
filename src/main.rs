893e00a703ea23525bd6bb9be7787fee2cff434f
cursedcadaver <collinschacha@hotmail.com>
2024-07-28 06:50:36 +0300

    added help functionality

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-    match args[1].as_str() {
-        "hello" => println!("hello"),
-        "goodbye" => println!("goodbye"),
-        _ => println!("unknown command"),
+    if args.len() == 0 {
+        help();
     }
+    match args[1].as_str() {
+        "init" => println!("hello"),
+        "run" => println!("goodbye"),
+        "watch" => println!("goodbye"),
+        _ => {
+            println!("unknown command");
+            help();
+        }
+    }
+}
+
+fn help() {
+    println!("Commands are init, watch and run")
 }
